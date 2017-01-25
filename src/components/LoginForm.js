81a6c4a311542f8cc4a95a636fcfd8d2937acef2
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-25 17:38:48 -0500

    enable firebase auth login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -9,6 +10,10 @@ class LoginForm extends Component {
   }
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+  onButtonPress() {
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password });
   }
   render() {
     return (
@@ -31,9 +36,12 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -41,12 +49,17 @@ class LoginForm extends Component {
     );
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password
-  };
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error } = auth;
+  return { email, password, error };
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
