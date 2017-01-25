d9595b66f31a0b56aaf20a6b1b2b7d22493df9d9
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-25 18:22:45 -0500

    add loading spinner on login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER
+ } from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -17,9 +23,11 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
-      .catch(() => {
+      .catch((error) => {
+        console.log(error);
         firebase.auth().createUserWithEmailAndPassword(email, password)
           .then(user => loginUserSuccess(dispatch, user))
           .catch(() => loginUserFail(dispatch));
