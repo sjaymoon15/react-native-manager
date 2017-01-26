376bfcee3b95f4ef189e5ab2c6ff2461a42865fb
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-26 14:49:24 -0500

    navigate to employeeList scene

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List</Text>
+        <Text>List</Text>
+        <Text>List</Text>
+        <Text>List</Text>
+        <Text>List</Text>
+        <Text>List</Text>
+      </View>
+    );
+  }
+}
+export default EmployeeList;
